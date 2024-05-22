7b9544f98df2064a034f1e2789338bb3aa9682fc
amanduquia <alejandra.1701914799@ucaldas.edu.co>
2024-05-22 18:57:01 -0500

    Funcionalidad con modals, tomar foto, mensajes incompletos y enviado

--- lib/screens/denuncia_foco_screen.dart
+++ lib/screens/denuncia_foco_screen.dart
@@ -1,14 +1,139 @@
 import 'package:flutter/material.dart';
-import '../widgets/denuncia_foco_form.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class DenunciaFocoScreen extends StatelessWidget {
+class DenunciaFocoScreen extends StatefulWidget {
+  @override
+  _DenunciaFocoScreenState createState() => _DenunciaFocoScreenState();
+}
+
+class _DenunciaFocoScreenState extends State<DenunciaFocoScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController descripcionController = TextEditingController();
+  final TextEditingController ubicacionController = TextEditingController();
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  void _showSuccessDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Éxito"),
+          content: Text("Sus datos se han enviado correctamente"),
+          actions: [
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showIncompleteDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Campos Incompletos"),
+          content: Text("Por favor complete todos los campos."),
+          actions: [
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _handleSubmit() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      // Aquí puedes hacer lo que necesites con los datos recopilados,
+      // como enviarlos a través de una solicitud HTTP.
+      _showSuccessDialog();
+    } else {
+      _showIncompleteDialog();
+    }
+  }
+
+  void _clearFields() {
+    descripcionController.clear();
+    ubicacionController.clear();
+    setState(() {
+      _image = null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Denunciar Foco'),
       ),
-      body: DenunciaFocoForm(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                controller: descripcionController,
+                decoration: InputDecoration(labelText: 'Descripción'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingrese una descripción';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: ubicacionController,
+                decoration: InputDecoration(labelText: 'Ubicación'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingrese una ubicación';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              Text('Ilustración:'),
+              _image == null
+                  ? Text('No se ha seleccionado una imagen.')
+                  : Image.file(_image!),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: _pickImage,
+                child: Text('Tomar Foto'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _handleSubmit,
+                child: Text('Enviar'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/widgets/denuncia_foco_form.dart
+++ lib/widgets/denuncia_foco_form.dart
@@ -9,6 +9,8 @@ class DenunciaFocoForm extends StatefulWidget {
 
 class _DenunciaFocoFormState extends State<DenunciaFocoForm> {
   final _formKey = GlobalKey<FormState>();
+  final TextEditingController descripcionController = TextEditingController();
+  final TextEditingController ubicacionController = TextEditingController();
   File? _image;
 
   Future<void> _pickImage() async {
@@ -22,59 +24,86 @@ class _DenunciaFocoFormState extends State<DenunciaFocoForm> {
     }
   }
 
+  void _showSuccessDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Éxito"),
+          content: Text("Sus datos se han enviado correctamente"),
+          actions: [
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _clearFields() {
+    descripcionController.clear();
+    ubicacionController.clear();
+    setState(() {
+      _image = null;
+    });
+  }
+
+  void _handleSubmit() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      _showSuccessDialog();
+      _clearFields();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Descripción'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese una descripción';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  // Guarda el valor de la descripción
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Ubicación'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese una ubicación';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  // Guarda el valor de la ubicación
-                },
-              ),
-              SizedBox(height: 20),
-              Text('Ilustración:'),
-              _image == null
-                  ? Text('No se ha seleccionado una imagen.')
-                  : Image.file(_image!),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: _pickImage,
-                child: Text('Tomar Foto'),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    // Procesa los datos del formulario
-                  }
-                },
-                child: Text('Enviar'),
-              ),
-             ],
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            TextFormField(
+              controller: descripcionController,
+              decoration: InputDecoration(labelText: 'Descripción'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor ingrese una descripción';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: ubicacionController,
+              decoration: InputDecoration(labelText: 'Ubicación'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor ingrese una ubicación';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 20),
+            Text('Ilustración:'),
+            _image == null
+                ? Text('No se ha seleccionado una imagen.')
+                : Image.file(_image!),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _pickImage,
+              child: Text('Tomar Foto'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _handleSubmit,
+              child: Text('Enviar'),
+            ),
+          ],
         ),
       ),
     );
