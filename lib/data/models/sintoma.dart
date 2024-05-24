ae0e90389f9c4c6b6c2dd069c94f62d3ac55f2e9
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2024-05-23 19:54:54 -0500

    se crea la clase sintoma

---
+++ lib/data/models/sintoma.dart
@@ -0,0 +1,11 @@
+class Sintoma {
+  final int id;
+  final String nombre;
+  final String descripcion;
+
+  Sintoma({
+    required this.id,
+    required this.nombre,
+    required this.descripcion,
+  });
+}
