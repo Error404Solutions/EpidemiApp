9efb9fceef2b76cbf23032ded236949920eae937
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2024-05-23 21:40:02 -0500

    entidad medicamento

---
+++ lib/automedicacion/domain/entities/medicamento.dart
@@ -0,0 +1,13 @@
+class Medicamento {
+  final int id;
+  final String nombre;
+  final String descripcion;
+  final bool esProhibido;
+
+  Medicamento({
+    required this.id,
+    required this.nombre,
+    required this.descripcion,
+    required this.esProhibido,
+  });
+}
