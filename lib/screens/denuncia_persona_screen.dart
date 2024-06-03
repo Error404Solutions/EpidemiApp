86a7d94061bfa421e7ce1485710ce9ff3ece6070
amanduquia <alejandra.1701914799@ucaldas.edu.co>
2024-06-02 21:49:35 -0500

    Últimos cambios rama aleja

--- lib/screens/denuncia_persona_screen.dart
+++ lib/screens/denuncia_persona_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../widgets/denuncia_persona_form.dart';
 
 class DenunciaPersonaScreen extends StatefulWidget {
   @override
@@ -12,6 +11,8 @@ class _DenunciaPersonaScreenState extends State<DenunciaPersonaScreen> {
   final TextEditingController apellidoController = TextEditingController();
   final TextEditingController sintomasController = TextEditingController();
   final TextEditingController ubicacionController = TextEditingController();
+
+  DateTime? selectedDate; // Variable to store selected date
 
   void _showSuccessDialog() {
     showDialog(
@@ -39,7 +40,67 @@ class _DenunciaPersonaScreenState extends State<DenunciaPersonaScreen> {
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text("Campos Incompletos"),
-          content: Text("Por favor complete todos los campos."),
+          content: Text("Por favor complete correctamente todos los campos."),
+          actions: [
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showNameErrorDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Nombre Incorrecto"),
+          content: Text("Ingrese un nombre con máximo 100 caracteres."),
+          actions: [
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showLastNameErrorDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Apellido Incorrecto"),
+          content: Text("Ingrese un apellido con máximo 100 caracteres."),
+          actions: [
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDateErrorDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Fecha Incorrecta"),
+          content: Text("Por favor ingresa la fecha actual de la denuncia."),
           actions: [
             TextButton(
               child: Text("OK"),
@@ -55,14 +116,37 @@ class _DenunciaPersonaScreenState extends State<DenunciaPersonaScreen> {
 
   void _handleSubmit() {
     if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      // Aquí puedes hacer lo que necesites con los datos recopilados,
-      // como enviarlos a través de una solicitud HTTP.
-      _showSuccessDialog();
+      // Checking if name length is greater than 100
+      if (nombreController.text.length > 100) {
+        _showNameErrorDialog();
+      }
+      // Checking if last name length is greater than 100
+      else if (apellidoController.text.length > 100) {
+        _showLastNameErrorDialog();
+      }
+      // Checking if the selected date is not equal to the current date
+      else if (_isDateDifferentFromToday(selectedDate)) {
+        _showDateErrorDialog();
+      } else {
+        _formKey.currentState!.save();
+        // Aquí puedes hacer lo que necesites con los datos recopilados,
+        // como enviarlos a través de una solicitud HTTP.
+        _showSuccessDialog();
+      }
     } else {
       _showIncompleteDialog();
     }
   }
+
+  // Function to check if the selected date is different from today
+  bool _isDateDifferentFromToday(DateTime? date) {
+    if (date == null) return true;
+
+    final today = DateTime.now();
+    final selectedDate = DateTime(date.year, date.month, date.day);
+    return selectedDate != DateTime(today.year, today.month, today.day);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -117,6 +201,18 @@ class _DenunciaPersonaScreenState extends State<DenunciaPersonaScreen> {
                 },
               ),
               SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  _selectDate(context);
+                },
+                child: AbsorbPointer(
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Fecha del Contagio'),
+                    controller: TextEditingController(text: selectedDate != null ? selectedDate.toString() : ''),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _handleSubmit,
                 child: Text('Enviar'),
@@ -126,5 +222,20 @@ class _DenunciaPersonaScreenState extends State<DenunciaPersonaScreen> {
         ),
       ),
     );
+  }
+
+  // Function to open date picker
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
   }
 }
