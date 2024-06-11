fc639a6ad5571ce1a76a708df383c35a58177aec
AlejandroLealGonzalez <alejandro.leal05@outlook.com>
2024-06-10 21:56:31 -0500

    Actualizando exportaciones de archivo de barrido

--- lib/auto/presentation/screens/screens.dart
+++ lib/auto/presentation/screens/screens.dart
@@ -4,6 +4,5 @@ export 'package:flutter_application_1/auto/presentation/screens/discover/discove
 export 'package:flutter_application_1/auto/presentation/screens/animated/animated_screen.dart';
 export 'package:flutter_application_1/auto/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:flutter_application_1/auto/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
-export 'package:flutter_application_1/auto/presentation/screens/progress/progress_screen.dart';
 export 'package:flutter_application_1/auto/presentation/screens/ui_controls/ui_controls_screen.dart';
 export 'package:flutter_application_1/auto/presentation/screens/home/home_screen.dart';
